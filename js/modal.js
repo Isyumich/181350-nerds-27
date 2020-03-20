34a8b716a1b5b0eb8ab09a984a061f22ac01b33c
Isyumich <59385945+Isyumich@users.noreply.github.com>
2020-03-20 10:39:25 +0300

    Починил анимацию при закрытии окна

--- js/modal.js
+++ js/modal.js
@@ -11,14 +11,26 @@ var close = document.querySelector(".modal-close");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-active");
-  popup.classList.remove("modal-close-animation");
+  if (popup.classList.contains("modal-close-animation")) {
+    popup.classList.remove("modal-close-animation");
+  }
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-close-animation");
-  popup.classList.remove("modal-active");
-  popup.classList.remove("modal-error");
+  if (popup.classList.contains("modal-error")) {
+    popup.classList.remove("modal-error");
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  setTimeout( function () {
+    if (popup.classList.contains("modal-active")) {
+      popup.classList.remove("modal-active");
+    }
+  }, 550)
 });
 
 form.addEventListener("submit", function (evt) {
